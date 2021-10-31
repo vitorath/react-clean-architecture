4c6d5a754af6e03e288be2ba8c80617a227be4a6
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-10-31 10:51:29 -0300

    feat: ensure RemoteAuthentication returns and Account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,17 +3,17 @@ import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { AccountModel } from '@/domain/models/account-model'
-import { AuthenticationParams } from '@/domain/usecases/authentication'
+import { Authentication, AuthenticationParams } from '@/domain/usecases/authentication'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpClient: HttpPostClient<AuthenticationParams, AccountModel>) { }
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpClient.post({ url: this.url, body: params })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return await Promise.resolve()
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.unauthorized: throw new InvalidCredentialError()
       default: throw new UnexpectedError()
     }
