6e6c8276d2789b884ca49cb0bca614c0c150fd73
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-11-06 18:54:27 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -20,11 +20,11 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '🔴'
+    return errorMessage ? '🔴' : '🟢'
   }
 
   const getTitle = (): string => {
-    return errorMessage
+    return errorMessage || 'Tudo certo!'
   }
 
   return (
