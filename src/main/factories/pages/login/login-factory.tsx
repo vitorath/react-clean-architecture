7e33271f4df057629d28e2a869e057045446d95d
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-11-12 17:39:11 -0300

    test: ensure LoginValidationComposite uses correct validations

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
-import { makeLoginValidationFactory } from './login-validation-factory'
+import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       authentication={makeRemoteAuthentication()}
-      validation={makeLoginValidationFactory()}
+      validation={makeLoginValidation()}
     />
   )
 }
