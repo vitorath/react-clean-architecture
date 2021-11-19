2fe70e9155178fd03317efb29fce36a797a9bef4
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-11-19 04:10:58 -0300

    feat: ensure RemoteAddAccount returns an AccountModel on success

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -12,7 +12,7 @@ export class RemoteAddAccount implements AddAccount {
   async add (params: AddAccountParams): Promise<AccountModel> {
     const httpResponse = await this.httpClient.post({ url: this.url, body: params })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return null
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.forbidden: throw new EmailInUseError()
       default: throw new UnexpectedError()
     }
