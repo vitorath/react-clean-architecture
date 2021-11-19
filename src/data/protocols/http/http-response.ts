6ac5ed5e9b554cb19dd3643258d4f708e7e00acc
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-11-19 04:05:02 -0300

    feat: ensure RemoteAddAccount thorws UnexpectedError if HttpClient returns 400

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -2,7 +2,7 @@ export enum HttpStatusCode {
   ok = 200,
   noContent = 204,
   badRequest = 400,
-  forbidden = 400,
+  forbidden = 403,
   notFound = 404,
   unauthorized = 401,
   serverError = 500
--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,5 +1,5 @@
 import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
-import { EmailInUseError } from '@/domain/errors'
+import { EmailInUseError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
 import { AddAccount, AddAccountParams } from '@/domain/usecases'
 
@@ -12,8 +12,9 @@ export class RemoteAddAccount implements AddAccount {
   async add (params: AddAccountParams): Promise<AccountModel> {
     const httpResponse = await this.httpClient.post({ url: this.url, body: params })
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return null
       case HttpStatusCode.forbidden: throw new EmailInUseError()
-      default: return null
+      default: throw new UnexpectedError()
     }
   }
 }
