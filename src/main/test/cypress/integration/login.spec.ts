12f409b3315d199d0608a466f6334c21f5958e17
Vitor Thomazini <vitor.thomazini94@gmail.com>
2021-12-26 19:58:37 -0300

    test: ensure Login will not submit form if its invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -124,4 +124,15 @@ describe('Login', () => {
     cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     cy.url().should('eq', `${baseUrl}/login`)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    cy.intercept(loginServerStub, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
